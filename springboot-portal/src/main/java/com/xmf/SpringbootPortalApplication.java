c3fd7a7246c0a397ca0d3706ab5a0954aa05e39b
笨小孩_好孩子 <835399066@qq.com>
2017-11-20 12:25:42 +0800

    测试上传项目

---
+++ springboot-portal/src/main/java/com/xmf/SpringbootPortalApplication.java
@@ -0,0 +1,12 @@
+package com.xmf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPortalApplication.class, args);
+	}
+}
